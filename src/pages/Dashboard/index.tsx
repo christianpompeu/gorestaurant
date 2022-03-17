89d3a92a6a8b1c02b69c35cb49b1fc4b4bd4816c
Christian <christianpompeu@gmail.com>
2022-03-17 13:27:04 -0300

    addingfood

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,19 +1,33 @@
 import { useState } from "react";
 import Header from "../../components/Header";
 import ModalAddFood from "../../components/ModalAddFood";
+import api from "../../services/api";
+import { Food, Foods } from "../../types";
 
 function Dashboard() {
   const [modalOpen, setModalOpen] = useState(false);
+  const [foods, setFoods] = useState<Foods>();
 
   function toggleModal() {
     setModalOpen(!modalOpen);
   }
+
+  async function handleAddFood(food: Food) {
+    try {
+      const response = await api.post("/foods", {
+        ...food,
+        available: true,
+      });
+
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <>
       <Header openModal={toggleModal} />
-      <ModalAddFood
-        isOpen={modalOpen}
-      />
+      <ModalAddFood isOpen={modalOpen} setIsOpen={toggleModal} />
     </>
   );
 }
--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,15 @@
-export interface FoodInterface {
+export interface Food {
   id: BigInt;
   name: string;
   description: string;
   price: number;
   available: boolean;
   image: string;
+}
+
+export interface Foods {
+  foods: Food[];
+  edditingFood: Food;
+  modalOpen: boolean;
+  editModalOpen: boolean;
 }
